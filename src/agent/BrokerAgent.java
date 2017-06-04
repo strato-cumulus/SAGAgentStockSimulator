d86a2abd0031888103a9131ffbdb9bb013c81810
strato <kuba10@gmail.com>
2017-06-04 13:16:05 +0200

    gson

---
+++ src/agent/BrokerAgent.java
@@ -0,0 +1,8 @@
+package agent;
+
+import jade.core.Agent;
+
+public class BrokerAgent extends Agent {
+
+
+}
--- src/agent/PlayerAgent.java
+++ src/agent/PlayerAgent.java
@@ -1,23 +1,20 @@
 package agent;
 
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import model.Stock;
 
 public class PlayerAgent extends Agent {
 
     @Override
     protected void setup() {
         super.setup();
-        addBehaviour(new Behaviour() {
-            @Override
-            public void action() {
+    }
 
-            }
-
-            @Override
-            public boolean done() {
-                return false;
-            }
-        });
+    protected ACLMessage createOfferMessage(Stock stock, int amount, int unitPrice) {
+        ACLMessage aclMessage = new ACLMessage(ACLMessage.PROPOSE);
+        aclMessage.setSender(this.getAID());
+        aclMessage.setContent(String.format("{stock: %s, amount: %d, unitPrice: %d}", stock, amount, unitPrice));
+        return aclMessage;
     }
 }
