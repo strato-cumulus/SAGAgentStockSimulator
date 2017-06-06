8428edd0ae176050d08b815134c057a26bdc9692
strato <kuba10@gmail.com>
2017-06-06 21:39:46 +0200

    migracja share->stock, portfolio

--- src/model/order/BuyOrder.java
+++ src/model/order/BuyOrder.java
@@ -5,7 +5,7 @@ import model.Stock;
 
 public class BuyOrder extends Order {
 
-    public BuyOrder(Stock stock, int quantity, AID buyerAID) {
-        super(stock, quantity, buyerAID);
+    public BuyOrder(Stock stock, int quantity, String buyerName) {
+        super(stock, quantity, buyerName);
     }
 }
