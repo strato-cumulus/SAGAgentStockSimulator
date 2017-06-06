d915415f25cfc0f8ad0e9c4d9b1a037f79813e7c
Marcin Frankowski <marcinfrank1@gmail.com>
2017-06-06 22:34:57 +0200

    equilibrium price computing

--- src/controller/manager/TransactionManager.java
+++ src/controller/manager/TransactionManager.java
@@ -12,7 +12,7 @@ import model.order.SellOrder;
 import model.request.CommitTransactionRequest;
 import model.transaction.Transaction;
 
-import java.util.List;
+import java.util.*;
 
 public class TransactionManager extends CyclicBehaviour {
 
@@ -58,5 +58,101 @@ public class TransactionManager extends CyclicBehaviour {
                 }
             }
         }
+    }
+
+    private int getEquilibriumPrice(String tickerCode) {
+        // <cena, ilosc>
+        SortedMap<Integer, Integer> buyOrdersMap = new TreeMap<>(Collections.reverseOrder());
+        SortedMap<Integer, Integer> sellOrdersMap = new TreeMap<>();
+        Map<Integer, Integer> supplyMap = new HashMap<>();
+        Map<Integer, Integer> demandMap = new HashMap<>();
+        Integer distribution = 0;
+
+        for(BuyOrder buyOrder : buyOrders) {
+            Integer value;
+            if(buyOrder.getStock().getTickerCode() == tickerCode) {
+                value = buyOrdersMap.putIfAbsent(buyOrder.getTotalPrice(), buyOrder.getQuantity());
+                if (value != null) {
+                    buyOrdersMap.put(buyOrder.getTotalPrice(), value + buyOrder.getQuantity());
+                }
+            }
+        }
+
+        for(SellOrder sellOrder : sellOrders) {
+            Integer value;
+            if(sellOrder.getStock().getTickerCode() == tickerCode) {
+                value = sellOrdersMap.putIfAbsent(sellOrder.getTotalPrice(), sellOrder.getQuantity());
+                if (value != null) {
+                    sellOrdersMap.put(sellOrder.getTotalPrice(), value + sellOrder.getQuantity());
+                }
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> entry : sellOrdersMap.entrySet()) {
+            distribution = distribution + distribution + entry.getValue();
+            supplyMap.put(entry.getKey(),distribution);
+        }
+
+        for(Map.Entry<Integer,Integer> entry : buyOrdersMap.entrySet()) {
+            distribution = distribution + distribution + entry.getValue();
+            supplyMap.put(entry.getKey(),distribution);
+        }
+
+        List<Integer> supplyPrice = new LinkedList<>();
+        supplyPrice.addAll(supplyMap.keySet());
+
+        List<Integer> demendPrice = new LinkedList<>();;
+        demendPrice.addAll(demandMap.keySet());
+
+        Set<Integer> computingSet = new HashSet<Integer>();
+        computingSet.addAll(supplyPrice);
+        computingSet.addAll(demendPrice);
+        Set<Integer> sortedSet = new TreeSet<>();
+        sortedSet.addAll(computingSet);
+
+        List<Integer> sortedList = new ArrayList<>(sortedSet);
+
+        for(int i = 0; i < sortedList.size(); i++) {
+            Integer supplyQuantity = supplyMap.get(sortedList.get(i));
+            if(supplyQuantity == null) continue;
+            Integer demandQuantity = demandMap.get(sortedList.get(i));
+            if(demandQuantity == null) demandQuantity = Integer.MAX_VALUE;
+
+            if(supplyQuantity == demandQuantity) {
+                return sortedList.get(i);
+            }
+
+            if(supplyQuantity < demandQuantity) {
+                int j;
+                int k;
+                for (j = i+1; supplyMap.get(sortedList.get(j)) == null && j < sortedList.size(); j++);
+                for (k = i+1; demandMap.get(sortedList.get(k)) == null && k < sortedList.size(); k++);
+                Integer nextSupplyQuantity = supplyMap.get(sortedList.get(j));
+                Integer nextDemandQuantity = demandMap.get(sortedList.get(k));
+                if(nextDemandQuantity < nextSupplyQuantity) {
+                    return 1;
+                }
+            }
+        }
+
+        for(int i = sortedList.size() - 1; i >= 0; i--) {
+            Integer supplyQuantity = supplyMap.get(sortedList.get(i));
+            if(supplyQuantity == null) supplyQuantity = Integer.MAX_VALUE;;
+            Integer demandQuantity = demandMap.get(sortedList.get(i));
+            if(demandQuantity == null) continue;
+
+            if(demandQuantity < supplyQuantity) {
+                int j;
+                int k;
+                for (j = i-1; supplyMap.get(sortedList.get(j)) == null && j >= 0; j--);
+                for (k = i-1; demandMap.get(sortedList.get(k)) == null && k >= 0; k--);
+                Integer nextSupplyQuantity = supplyMap.get(sortedList.get(j));
+                Integer nextDemandQuantity = demandMap.get(sortedList.get(k));
+                if(nextDemandQuantity > nextSupplyQuantity) {
+                    return 1;
+                }
+            }
+        }
+        return 1;
     }
 }
