e9842c802f6a0d717a4a03aba4f7da961c9be6a0
FGralewski <filip.gralewski@gmail.com>
2017-06-12 23:23:04 +0200

    TickerBehaviour for calculating equilibrium price

--- src/agent/BrokerAgent.java
+++ src/agent/BrokerAgent.java
@@ -11,6 +11,8 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import model.Ontology;
 import model.Portfolio;
+import model.math.Line;
+import model.math.Point;
 import model.order.BuyOrder;
 import model.order.SellOrder;
 import model.request.BlockFundsRequest;
@@ -20,8 +22,7 @@ import resource.ResourceCreationException;
 import resource.data.FileShareCreator;
 import resource.data.ShareCreator;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BrokerAgent extends Agent {
 
@@ -165,6 +166,17 @@ public class BrokerAgent extends Agent {
                 return false;
             }
         });
+
+        addBehaviour(new TickerBehaviour(this, 500) {
+            @Override
+            protected void onTick() {
+                Map<String, Integer> newPrices = new HashMap<String, Integer>();
+                for (String stock : equilibrium.equilibriumPrice.keySet()) {
+                    newPrices.put(stock, getEquilibriumPrice(stock));
+                }
+                equilibrium.updatePrices(newPrices);
+            }
+        });
     }
 
     protected void initializeShares() {
@@ -175,6 +187,114 @@ public class BrokerAgent extends Agent {
         } catch (ResourceCreationException e) {
             e.printStackTrace();
         }
+    }
+
+    private int getEquilibriumPrice(String tickerCode) {
+        // <cena, ilosc>
+        SortedMap<Integer, Integer> buyOrdersMap = new TreeMap<>(Collections.reverseOrder());
+        SortedMap<Integer, Integer> sellOrdersMap = new TreeMap<>();
+        Map<Integer, Integer> supplyMap = new HashMap<>();
+        Map<Integer, Integer> demandMap = new HashMap<>();
+        Point A1 = new Point();
+        Point B1 = new Point();
+        Point A2 = new Point();
+        Point B2 = new Point();
+        Integer distribution = 0;
+
+        for(BuyOrder buyOrder : buyOrders) {
+            Integer value;
+            if(buyOrder.getStock() == tickerCode) {
+                value = buyOrdersMap.putIfAbsent(buyOrder.getTotalPrice(), buyOrder.getQuantity());
+                if (value != null) {
+                    buyOrdersMap.put(buyOrder.getTotalPrice(), value + buyOrder.getQuantity());
+                }
+            }
+        }
+
+        for(SellOrder sellOrder : sellOrders) {
+            Integer value;
+            if(sellOrder.getStock() == tickerCode) {
+                value = sellOrdersMap.putIfAbsent(sellOrder.getTotalPrice(), sellOrder.getQuantity());
+                if (value != null) {
+                    sellOrdersMap.put(sellOrder.getTotalPrice(), value + sellOrder.getQuantity());
+                }
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> entry : sellOrdersMap.entrySet()) {
+            distribution = distribution + distribution + entry.getValue();
+            supplyMap.put(entry.getKey(),distribution);
+        }
+
+        for(Map.Entry<Integer,Integer> entry : buyOrdersMap.entrySet()) {
+            distribution = distribution + distribution + entry.getValue();
+            supplyMap.put(entry.getKey(),distribution);
+        }
+
+        List<Integer> supplyPrice = new LinkedList<>();
+        supplyPrice.addAll(supplyMap.keySet());
+
+        List<Integer> demandPrice = new LinkedList<>();
+        demandPrice.addAll(demandMap.keySet());
+
+        Set<Integer> computingSet = new HashSet<>();
+        computingSet.addAll(supplyPrice);
+        computingSet.addAll(demandPrice);
+        Set<Integer> sortedSet = new TreeSet<>();
+        sortedSet.addAll(computingSet);
+
+        List<Integer> sortedList = new ArrayList<>(sortedSet);
+
+        for(int i = 0; i < sortedList.size(); i++) {
+            Integer supplyQuantity = supplyMap.get(sortedList.get(i));
+            if(supplyQuantity == null) continue;
+            Integer demandQuantity = demandMap.get(sortedList.get(i));
+            if(demandQuantity == null) demandQuantity = Integer.MAX_VALUE;
+
+            if(supplyQuantity == demandQuantity) {
+                return sortedList.get(i);
+            }
+
+            if(supplyQuantity < demandQuantity) {
+                int j;
+                int k;
+                for (j = i+1; supplyMap.get(sortedList.get(j)) == null && j < sortedList.size(); j++);
+                for (k = i+1; demandMap.get(sortedList.get(k)) == null && k < sortedList.size(); k++);
+                Integer nextSupplyQuantity = supplyMap.get(sortedList.get(j));
+                Integer nextDemandQuantity = demandMap.get(sortedList.get(k));
+                if(nextDemandQuantity < nextSupplyQuantity) {
+                    A1 = new Point(supplyQuantity.doubleValue(), sortedList.get(i).doubleValue());
+                    B1 = new Point(nextSupplyQuantity.doubleValue(), sortedList.get(j).doubleValue());
+                }
+            }
+        }
+
+        for(int i = sortedList.size() - 1; i >= 0; i--) {
+            Integer supplyQuantity = supplyMap.get(sortedList.get(i));
+            if(supplyQuantity == null) supplyQuantity = Integer.MAX_VALUE;;
+            Integer demandQuantity = demandMap.get(sortedList.get(i));
+            if(demandQuantity == null) continue;
+
+            if(demandQuantity < supplyQuantity) {
+                int j;
+                int k;
+                for (j = i-1; supplyMap.get(sortedList.get(j)) == null && j >= 0; j--);
+                for (k = i-1; demandMap.get(sortedList.get(k)) == null && k >= 0; k--);
+                Integer nextSupplyQuantity = supplyMap.get(sortedList.get(j));
+                Integer nextDemandQuantity = demandMap.get(sortedList.get(k));
+                if(nextDemandQuantity > nextSupplyQuantity) {
+                    A2 = new Point(demandQuantity.doubleValue(), sortedList.get(i).doubleValue());
+                    B2 = new Point(nextDemandQuantity.doubleValue(), sortedList.get(k).doubleValue());
+                }
+            }
+        }
+        return intersection(A1, B1, A2, B2);
+    }
+
+    private int intersection (Point A1, Point B1, Point A2, Point B2 ) {
+        Line LA = Line.getLineFromPoints(A1, B1);
+        Line LB = Line.getLineFromPoints(A2, B2);
+        return LA.getIntersection(LB).getY().intValue();
     }
 
 }
